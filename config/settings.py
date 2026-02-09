3d0e056952ab61a7c83b4f0054561b51433dafe8
4khello <4khello@email.com>
2026-02-09 03:10:07 +0200

    Fix CSRF trusted origins and update admin panel

--- config/settings.py
+++ config/settings.py
@@ -123,3 +123,8 @@ DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 LOGIN_URL = "/login/"
 LOGIN_REDIRECT_URL = "/"
 LOGOUT_REDIRECT_URL = "/login/"
+
+CSRF_TRUSTED_ORIGINS = [
+    "https://supervision-system-production.up.railway.app",
+    "https://*.up.railway.app" # للسماح بأي رابط فرعي من ريل واي
+]
